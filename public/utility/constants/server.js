6fba259747e4381b0f4e38c05def96ba45453dfe
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-29 20:40:16 +0200

    Update server.js

--- public/utility/constants/server.js
+++ public/utility/constants/server.js
@@ -1,13 +1,13 @@
 import { ipv4 } from "../network/ipv4-info";
 
 // Server
-export const IP_ADDRESS: string = ipv4;
-export const PORT: number = 3000;
+export const IP_ADDRESS = ipv4;
+export const PORT = 3000;
 
 // Directories
-export const DIRECTORY_PUBLIC: string = "public";
-export const PATH_VIEW_INDEX: string = "./views/index.html";
+export const DIRECTORY_PUBLIC = "public";
+export const PATH_VIEW_INDEX = "./views/index.html";
 
 // SSl
-export const PATH_SSL_KEY: string = "./ssl/private.key";
-export const PATH_SSL_CERT: string = "./ssl/certificate.crt";
+export const PATH_SSL_KEY = "./ssl/private.key";
+export const PATH_SSL_CERT = "./ssl/certificate.crt";
